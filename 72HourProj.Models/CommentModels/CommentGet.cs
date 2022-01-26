51928fdd28d9d4234123e346effc65cf6bb23b1f
Jarae Gibson <jaraegibs@gmail.com>
2022-01-26 17:30:59 -0500

    added comments section minus get by id

---
+++ 72HourProj.Models/CommentModels/CommentGet.cs
@@ -0,0 +1,19 @@
+﻿using _72HourProj.Data.PostEntity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72HourProj.Models.CommentModels
+{
+    public class CommentGet
+    {
+        public int CommentId { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+        public string Text { get; set; }
+
+    }
+}
---
+++ 72HourProj.WebAPI/Controllers/CommentController.cs
@@ -0,0 +1,46 @@
+﻿using _72HourProj.Models.CommentModels;
+using _72HourProj.Services.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _72HourProj.WebAPI.Controllers
+{
+    [Authorize]
+    public class CommentController : ApiController
+    {
+        private CommentService CreateCommentService()
+        {
+            var authorId = Guid.Parse(User.Identity.GetUserId());
+            var commentService = new CommentService(authorId);
+            return commentService;
+
+        }
+
+        public IHttpActionResult Post(CommentCreate comment)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateCommentService();
+
+            if (!service.CreateComment(comment))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Get()
+        {
+            CommentService commentService = CreateCommentService();
+            var comment = commentService.GetComments();
+            return Ok(comment);
+        }
+
+
+    }
+}
